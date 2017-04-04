b511d7a01f1051fbdf0debf1892addc3d5efcb94
jssPolls <jagadguru6969@gmail.com>
2017-04-04 20:10:52 +0530

    Create adSense.js

---
+++ app/components/adSense.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Ad extends React.Component {
+  componentDidMount () {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }
+
+render () {
+    return (
+      <div className='ad'>
+        <ins className='adsbygoogle'
+          style={{ display: 'block' }}
+          data-ad-client='ca-pub-4922821586133078'
+          data-ad-format='auto' />
+      </div>
+    );
+  }
+}
